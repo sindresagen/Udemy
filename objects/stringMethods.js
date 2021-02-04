22a664b74d6a5bbcf2965934858030154ebd3756
sindresagen <71556636+sindresagen@users.noreply.github.com>
2021-02-04 08:59:14 +0100

    Create stringMethods.js

---
+++ objects/stringMethods.js
@@ -0,0 +1,17 @@
+let myName = '  Sindre Sagen  ';
+
+// Length property
+console.log(myName.length);
+
+// Convert to uppercase
+console.log(myName.toUpperCase());
+
+// Convert to lower case
+console.log(myName.toLowerCase());
+
+// Includes method
+let password = 'abc123password098';
+console.log(password.includes('password'));
+
+// Trim
+console.log(myName.trim());
